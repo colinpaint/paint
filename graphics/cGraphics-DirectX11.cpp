4ca296d33f3befb71201392b6557b4e3c616f364
colinporth <colinporth@users.noreply.github.com>
2021-08-13 12:49:04 +0100

    much closer io win32 using wrapper, need to communicate context from platform to graphics

--- graphics/cGraphics-DirectX11.cpp
+++ graphics/cGraphics-DirectX11.cpp
@@ -20,8 +20,6 @@ using namespace fmt;
 namespace {
   ID3D11Device* gDevice = NULL;
   ID3D11DeviceContext* gDeviceContext =NULL;
-  IDXGISwapChain* gSwapChain = NULL;
-  ID3D11RenderTargetView* gMainRenderTargetView = NULL;
 
   //{{{
   struct sBackendData {
@@ -749,106 +747,10 @@ namespace {
     ImGui::DestroyPlatformWindows();
     }
   //}}}
-
-  //{{{
-  void createRenderTarget() {
-    ID3D11Texture2D* backBuffer;
-    gSwapChain->GetBuffer (0, IID_PPV_ARGS (&backBuffer));
-    gDevice->CreateRenderTargetView (backBuffer, NULL, &gMainRenderTargetView);
-    backBuffer->Release();
-    }
-  //}}}
-  //{{{
-  void cleanupRenderTarget() {
-    if (gMainRenderTargetView) {
-      gMainRenderTargetView->Release();
-      gMainRenderTargetView = NULL;
-      }
-    }
-  //}}}
-  //{{{
-  LRESULT WINAPI WndProc (HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-
-    //if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
-    //  return true;
-
-    switch (msg) {
-      case WM_SIZE: if (gDevice != NULL && wParam != SIZE_MINIMIZED) {
-        cleanupRenderTarget();
-        gSwapChain->ResizeBuffers (0, (UINT)LOWORD(lParam), (UINT)HIWORD(lParam), DXGI_FORMAT_UNKNOWN, 0);
-        createRenderTarget();
-        }
-        return 0;
-
-      case WM_SYSCOMMAND:
-        if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
-          return 0;
-        break;
-
-      case WM_DESTROY:
-        ::PostQuitMessage (0);
-        return 0;
-
-      case WM_DPICHANGED:
-        if (ImGui::GetIO().ConfigFlags & ImGuiConfigFlags_DpiEnableScaleViewports) {
-          //const int dpi = HIWORD(wParam);
-          //printf("WM_DPICHANGED to %d (%.0f%%)\n", dpi, (float)dpi / 96.0f * 100.0f);
-          const RECT* suggested_rect = (RECT*)lParam;
-          ::SetWindowPos (hWnd, NULL, 
-                          suggested_rect->left, suggested_rect->top, 
-                          suggested_rect->right - suggested_rect->left, 
-                          suggested_rect->bottom - suggested_rect->top, 
-                          SWP_NOZORDER | SWP_NOACTIVATE);
-          }
-        break;
-      }
-
-    return ::DefWindowProc (hWnd, msg, wParam, lParam);
-    }
-  //}}}
   }
 
 //{{{
 bool cGraphics::init() {
-
-  //ImGui_ImplWin32_EnableDpiAwareness();
-  WNDCLASSEX wc = { sizeof(WNDCLASSEX),
-                    CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL),
-                    NULL, NULL, NULL, NULL, _T("ImGui Example"), NULL };
-  ::RegisterClassEx(&wc);
-
-  HWND hWnd = ::CreateWindow (wc.lpszClassName, 
-                             _T("Dear ImGui DirectX11 Example"), WS_OVERLAPPEDWINDOW,
-                              100, 100, 1280, 800, 
-                              NULL, NULL, wc.hInstance, NULL);
-
-  // Setup swap chain
-  DXGI_SWAP_CHAIN_DESC sd;
-  ZeroMemory(&sd, sizeof(sd));
-  sd.BufferCount = 2;
-  sd.BufferDesc.Width = 0;
-  sd.BufferDesc.Height = 0;
-  sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-  sd.BufferDesc.RefreshRate.Numerator = 60;
-  sd.BufferDesc.RefreshRate.Denominator = 1;
-  sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
-  sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-  sd.OutputWindow = hWnd;
-  sd.SampleDesc.Count = 1;
-  sd.SampleDesc.Quality = 0;
-  sd.Windowed = TRUE;
-  sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
-
-  UINT createDeviceFlags = 0; // = D3D11_CREATE_DEVICE_DEBUG;
-  D3D_FEATURE_LEVEL featureLevel;
-  const D3D_FEATURE_LEVEL featureLevelArray[2] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_0, };
-  if (D3D11CreateDeviceAndSwapChain (NULL, D3D_DRIVER_TYPE_HARDWARE, NULL,
-                                     createDeviceFlags, featureLevelArray, 2, D3D11_SDK_VERSION,
-                                     &sd, &gSwapChain, &gDevice,
-                                     &featureLevel, &gDeviceContext) != S_OK)
-    return false;
-
-  createRenderTarget();
 
   ImGuiIO& io = ImGui::GetIO();
   IM_ASSERT(io.BackendRendererUserData == NULL && "Already initialized a renderer backend!");
