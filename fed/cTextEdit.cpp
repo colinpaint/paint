0d3508ed7f157e79291739163ab0196bd756e9f4
colinporth <colinporth@users.noreply.github.com>
2021-09-19 19:35:14 +0100

    simpler

--- fed/cTextEdit.cpp
+++ fed/cTextEdit.cpp
@@ -93,104 +93,104 @@ namespace {
     0xff00ffff, // eScrollActive
     };
   //}}}
-   //{{{  language const
-   //{{{
-   const vector<string> kCKeywords = {
-     "alignas", "alignof", "and", "and_eq", "asm", "atomic_cancel", "atomic_commit", "atomic_noexcept", "auto",
-     "bitand", "bitor", "bool", "break",
-     "case", "catch", "char", "char16_t", "char32_t", "class", "compl", "concept",
-     "const", "constexpr", "const_cast", "continue",
-     "decltype", "default", "delete", "do", "double", "dynamic_cast",
-     "else", "enum", "explicit", "export", "extern",
-     "false", "float", "for", "friend",
-     "goto",
-     "if", "import", "inline", "int",
-     "long",
-     "module", "mutable",
-     "namespace", "new", "noexcept", "not", "not_eq", "nullptr",
-     "operator", "or", "or_eq",
-     "private", "protected", "public",
-     "register", "reinterpret_cast", "requires", "return",
-     "short", "signed", "sizeof", "static", "static_assert", "static_cast", "struct", "switch", "synchronized",
-     "template", "this", "thread_local", "throw", "true", "try", "typedef", "typeid", "typename",
-     "union", "unsigned", "using",
-     "virtual", "void", "volatile",
-     "wchar_t", "while",
-     "xor", "xor_eq"
-     };
-   //}}}
-   //{{{
-   const vector<string> kCBuiltIns = {
-     "abort", "abs", "acos", "asin", "atan", "atexit", "atof", "atoi", "atol",
-     "ceil", "clock", "cosh", "ctime",
-     "div",
-     "exit",
-     "fabs", "floor", "fmod",
-     "getchar", "getenv",
-     "isalnum", "isalpha", "isdigit", "isgraph", "ispunct", "isspace", "isupper",
-     "kbhit",
-     "log10", "log2", "log",
-     "map", "memcmp", "modf", "min", "max"
-     "pow", "printf", "putchar", "putenv", "puts",
-     "rand", "remove", "rename",
-     "sinh", "sqrt", "srand", "strcat", "strcmp", "strerror", "set", "std", "string", "sprintf", "snprintf",
-     "time", "tolower", "toupper",
-     "unordered_map", "unordered_set",
-     "vector",
-     };
-   //}}}
+  //{{{  language const
+  //{{{
+  const vector<string> kCKeywords = {
+    "alignas", "alignof", "and", "and_eq", "asm", "atomic_cancel", "atomic_commit", "atomic_noexcept", "auto",
+    "bitand", "bitor", "bool", "break",
+    "case", "catch", "char", "char16_t", "char32_t", "class", "compl", "concept",
+    "const", "constexpr", "const_cast", "continue",
+    "decltype", "default", "delete", "do", "double", "dynamic_cast",
+    "else", "enum", "explicit", "export", "extern",
+    "false", "float", "for", "friend",
+    "goto",
+    "if", "import", "inline", "int",
+    "long",
+    "module", "mutable",
+    "namespace", "new", "noexcept", "not", "not_eq", "nullptr",
+    "operator", "or", "or_eq",
+    "private", "protected", "public",
+    "register", "reinterpret_cast", "requires", "return",
+    "short", "signed", "sizeof", "static", "static_assert", "static_cast", "struct", "switch", "synchronized",
+    "template", "this", "thread_local", "throw", "true", "try", "typedef", "typeid", "typename",
+    "union", "unsigned", "using",
+    "virtual", "void", "volatile",
+    "wchar_t", "while",
+    "xor", "xor_eq"
+    };
+  //}}}
+  //{{{
+  const vector<string> kCBuiltIns = {
+    "abort", "abs", "acos", "asin", "atan", "atexit", "atof", "atoi", "atol",
+    "ceil", "clock", "cosh", "ctime",
+    "div",
+    "exit",
+    "fabs", "floor", "fmod",
+    "getchar", "getenv",
+    "isalnum", "isalpha", "isdigit", "isgraph", "ispunct", "isspace", "isupper",
+    "kbhit",
+    "log10", "log2", "log",
+    "map", "memcmp", "modf", "min", "max"
+    "pow", "printf", "putchar", "putenv", "puts",
+    "rand", "remove", "rename",
+    "sinh", "sqrt", "srand", "strcat", "strcmp", "strerror", "set", "std", "string", "sprintf", "snprintf",
+    "time", "tolower", "toupper",
+    "unordered_map", "unordered_set",
+    "vector",
+    };
+  //}}}
 
-   //{{{
-   const vector<string> kHlslKeywords = {
-     "AppendStructuredBuffer", "asm", "asm_fragment", "BlendState", "bool", "break", "Buffer", "ByteAddressBuffer", "case", "cbuffer", "centroid", "class", "column_major", "compile", "compile_fragment",
-     "CompileShader", "const", "continue", "ComputeShader", "ConsumeStructuredBuffer", "default", "DepthStencilState", "DepthStencilView", "discard", "do", "double", "DomainShader", "dword", "else",
-     "export", "extern", "false", "float", "for", "fxgroup", "GeometryShader", "groupshared", "half", "Hullshader", "if", "in", "inline", "inout", "InputPatch", "int", "interface", "line", "lineadj",
-     "linear", "LineStream", "matrix", "min16float", "min10float", "min16int", "min12int", "min16uint", "namespace", "nointerpolation", "noperspective", "NULL", "out", "OutputPatch", "packoffset",
-     "pass", "pixelfragment", "PixelShader", "point", "PointStream", "precise", "RasterizerState", "RenderTargetView", "return", "register", "row_major", "RWBuffer", "RWByteAddressBuffer", "RWStructuredBuffer",
-     "RWTexture1D", "RWTexture1DArray", "RWTexture2D", "RWTexture2DArray", "RWTexture3D", "sample", "sampler", "SamplerState", "SamplerComparisonState", "shared", "snorm", "stateblock", "stateblock_state",
-     "static", "string", "struct", "switch", "StructuredBuffer", "tbuffer", "technique", "technique10", "technique11", "texture", "Texture1D", "Texture1DArray", "Texture2D", "Texture2DArray", "Texture2DMS",
-     "Texture2DMSArray", "Texture3D", "TextureCube", "TextureCubeArray", "true", "typedef", "triangle", "triangleadj", "TriangleStream", "uint", "uniform", "unorm", "unsigned", "vector", "vertexfragment",
-     "VertexShader", "void", "volatile", "while",
-     "bool1","bool2","bool3","bool4","double1","double2","double3","double4", "float1", "float2", "float3", "float4", "int1", "int2", "int3", "int4", "in", "out", "inout",
-     "uint1", "uint2", "uint3", "uint4", "dword1", "dword2", "dword3", "dword4", "half1", "half2", "half3", "half4",
-     "float1x1","float2x1","float3x1","float4x1","float1x2","float2x2","float3x2","float4x2",
-     "float1x3","float2x3","float3x3","float4x3","float1x4","float2x4","float3x4","float4x4",
-     "half1x1","half2x1","half3x1","half4x1","half1x2","half2x2","half3x2","half4x2",
-     "half1x3","half2x3","half3x3","half4x3","half1x4","half2x4","half3x4","half4x4",
-   };
-   //}}}
-   //{{{
-   const vector<string> kHlslBuiltIns = {
-     "abort", "abs", "acos", "all", "AllMemoryBarrier", "AllMemoryBarrierWithGroupSync", "any", "asdouble", "asfloat", "asin", "asint", "asint", "asuint",
-     "asuint", "atan", "atan2", "ceil", "CheckAccessFullyMapped", "clamp", "clip", "cos", "cosh", "countbits", "cross", "D3DCOLORtoUBYTE4", "ddx",
-     "ddx_coarse", "ddx_fine", "ddy", "ddy_coarse", "ddy_fine", "degrees", "determinant", "DeviceMemoryBarrier", "DeviceMemoryBarrierWithGroupSync",
-     "distance", "dot", "dst", "errorf", "EvaluateAttributeAtCentroid", "EvaluateAttributeAtSample", "EvaluateAttributeSnapped", "exp", "exp2",
-     "f16tof32", "f32tof16", "faceforward", "firstbithigh", "firstbitlow", "floor", "fma", "fmod", "frac", "frexp", "fwidth", "GetRenderTargetSampleCount",
-     "GetRenderTargetSamplePosition", "GroupMemoryBarrier", "GroupMemoryBarrierWithGroupSync", "InterlockedAdd", "InterlockedAnd", "InterlockedCompareExchange",
-     "InterlockedCompareStore", "InterlockedExchange", "InterlockedMax", "InterlockedMin", "InterlockedOr", "InterlockedXor", "isfinite", "isinf", "isnan",
-     "ldexp", "length", "lerp", "lit", "log", "log10", "log2", "mad", "max", "min", "modf", "msad4", "mul", "noise", "normalize", "pow", "printf",
-     "Process2DQuadTessFactorsAvg", "Process2DQuadTessFactorsMax", "Process2DQuadTessFactorsMin", "ProcessIsolineTessFactors", "ProcessQuadTessFactorsAvg",
-     "ProcessQuadTessFactorsMax", "ProcessQuadTessFactorsMin", "ProcessTriTessFactorsAvg", "ProcessTriTessFactorsMax", "ProcessTriTessFactorsMin",
-     "radians", "rcp", "reflect", "refract", "reversebits", "round", "rsqrt", "saturate", "sign", "sin", "sincos", "sinh", "smoothstep", "sqrt", "step",
-     "tan", "tanh", "tex1D", "tex1D", "tex1Dbias", "tex1Dgrad", "tex1Dlod", "tex1Dproj", "tex2D", "tex2D", "tex2Dbias", "tex2Dgrad", "tex2Dlod", "tex2Dproj",
-     "tex3D", "tex3D", "tex3Dbias", "tex3Dgrad", "tex3Dlod", "tex3Dproj", "texCUBE", "texCUBE", "texCUBEbias", "texCUBEgrad", "texCUBElod", "texCUBEproj", "transpose", "trunc"
-   };
-   //}}}
+  //{{{
+  const vector<string> kHlslKeywords = {
+    "AppendStructuredBuffer", "asm", "asm_fragment", "BlendState", "bool", "break", "Buffer", "ByteAddressBuffer", "case", "cbuffer", "centroid", "class", "column_major", "compile", "compile_fragment",
+    "CompileShader", "const", "continue", "ComputeShader", "ConsumeStructuredBuffer", "default", "DepthStencilState", "DepthStencilView", "discard", "do", "double", "DomainShader", "dword", "else",
+    "export", "extern", "false", "float", "for", "fxgroup", "GeometryShader", "groupshared", "half", "Hullshader", "if", "in", "inline", "inout", "InputPatch", "int", "interface", "line", "lineadj",
+    "linear", "LineStream", "matrix", "min16float", "min10float", "min16int", "min12int", "min16uint", "namespace", "nointerpolation", "noperspective", "NULL", "out", "OutputPatch", "packoffset",
+    "pass", "pixelfragment", "PixelShader", "point", "PointStream", "precise", "RasterizerState", "RenderTargetView", "return", "register", "row_major", "RWBuffer", "RWByteAddressBuffer", "RWStructuredBuffer",
+    "RWTexture1D", "RWTexture1DArray", "RWTexture2D", "RWTexture2DArray", "RWTexture3D", "sample", "sampler", "SamplerState", "SamplerComparisonState", "shared", "snorm", "stateblock", "stateblock_state",
+    "static", "string", "struct", "switch", "StructuredBuffer", "tbuffer", "technique", "technique10", "technique11", "texture", "Texture1D", "Texture1DArray", "Texture2D", "Texture2DArray", "Texture2DMS",
+    "Texture2DMSArray", "Texture3D", "TextureCube", "TextureCubeArray", "true", "typedef", "triangle", "triangleadj", "TriangleStream", "uint", "uniform", "unorm", "unsigned", "vector", "vertexfragment",
+    "VertexShader", "void", "volatile", "while",
+    "bool1","bool2","bool3","bool4","double1","double2","double3","double4", "float1", "float2", "float3", "float4", "int1", "int2", "int3", "int4", "in", "out", "inout",
+    "uint1", "uint2", "uint3", "uint4", "dword1", "dword2", "dword3", "dword4", "half1", "half2", "half3", "half4",
+    "float1x1","float2x1","float3x1","float4x1","float1x2","float2x2","float3x2","float4x2",
+    "float1x3","float2x3","float3x3","float4x3","float1x4","float2x4","float3x4","float4x4",
+    "half1x1","half2x1","half3x1","half4x1","half1x2","half2x2","half3x2","half4x2",
+    "half1x3","half2x3","half3x3","half4x3","half1x4","half2x4","half3x4","half4x4",
+  };
+  //}}}
+  //{{{
+  const vector<string> kHlslBuiltIns = {
+    "abort", "abs", "acos", "all", "AllMemoryBarrier", "AllMemoryBarrierWithGroupSync", "any", "asdouble", "asfloat", "asin", "asint", "asint", "asuint",
+    "asuint", "atan", "atan2", "ceil", "CheckAccessFullyMapped", "clamp", "clip", "cos", "cosh", "countbits", "cross", "D3DCOLORtoUBYTE4", "ddx",
+    "ddx_coarse", "ddx_fine", "ddy", "ddy_coarse", "ddy_fine", "degrees", "determinant", "DeviceMemoryBarrier", "DeviceMemoryBarrierWithGroupSync",
+    "distance", "dot", "dst", "errorf", "EvaluateAttributeAtCentroid", "EvaluateAttributeAtSample", "EvaluateAttributeSnapped", "exp", "exp2",
+    "f16tof32", "f32tof16", "faceforward", "firstbithigh", "firstbitlow", "floor", "fma", "fmod", "frac", "frexp", "fwidth", "GetRenderTargetSampleCount",
+    "GetRenderTargetSamplePosition", "GroupMemoryBarrier", "GroupMemoryBarrierWithGroupSync", "InterlockedAdd", "InterlockedAnd", "InterlockedCompareExchange",
+    "InterlockedCompareStore", "InterlockedExchange", "InterlockedMax", "InterlockedMin", "InterlockedOr", "InterlockedXor", "isfinite", "isinf", "isnan",
+    "ldexp", "length", "lerp", "lit", "log", "log10", "log2", "mad", "max", "min", "modf", "msad4", "mul", "noise", "normalize", "pow", "printf",
+    "Process2DQuadTessFactorsAvg", "Process2DQuadTessFactorsMax", "Process2DQuadTessFactorsMin", "ProcessIsolineTessFactors", "ProcessQuadTessFactorsAvg",
+    "ProcessQuadTessFactorsMax", "ProcessQuadTessFactorsMin", "ProcessTriTessFactorsAvg", "ProcessTriTessFactorsMax", "ProcessTriTessFactorsMin",
+    "radians", "rcp", "reflect", "refract", "reversebits", "round", "rsqrt", "saturate", "sign", "sin", "sincos", "sinh", "smoothstep", "sqrt", "step",
+    "tan", "tanh", "tex1D", "tex1D", "tex1Dbias", "tex1Dgrad", "tex1Dlod", "tex1Dproj", "tex2D", "tex2D", "tex2Dbias", "tex2Dgrad", "tex2Dlod", "tex2Dproj",
+    "tex3D", "tex3D", "tex3Dbias", "tex3Dgrad", "tex3Dlod", "tex3Dproj", "texCUBE", "texCUBE", "texCUBEbias", "texCUBEgrad", "texCUBElod", "texCUBEproj", "transpose", "trunc"
+  };
+  //}}}
 
-   //{{{
-   const vector<string> kGlslKeywords = {
-     "auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern", "float", "for", "goto", "if", "inline", "int", "long", "register", "restrict", "return", "short",
-     "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while", "_Alignas", "_Alignof", "_Atomic", "_Bool", "_Complex", "_Generic", "_Imaginary",
-     "_Noreturn", "_Static_assert", "_Thread_local"
-   };
-   //}}}
-   //{{{
-   const vector<string> kGlslBuiltIns = {
-     "abort", "abs", "acos", "asin", "atan", "atexit", "atof", "atoi", "atol", "ceil", "clock", "cosh", "ctime", "div", "exit", "fabs", "floor", "fmod", "getchar", "getenv", "isalnum", "isalpha", "isdigit", "isgraph",
-     "ispunct", "isspace", "isupper", "kbhit", "log10", "log2", "log", "memcmp", "modf", "pow", "putchar", "putenv", "puts", "rand", "remove", "rename", "sinh", "sqrt", "srand", "strcat", "strcmp", "strerror", "time", "tolower", "toupper"
-   };
-   //}}}
-   //}}}
+  //{{{
+  const vector<string> kGlslKeywords = {
+    "auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern", "float", "for", "goto", "if", "inline", "int", "long", "register", "restrict", "return", "short",
+    "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while", "_Alignas", "_Alignof", "_Atomic", "_Bool", "_Complex", "_Generic", "_Imaginary",
+    "_Noreturn", "_Static_assert", "_Thread_local"
+  };
+  //}}}
+  //{{{
+  const vector<string> kGlslBuiltIns = {
+    "abort", "abs", "acos", "asin", "atan", "atexit", "atof", "atoi", "atol", "ceil", "clock", "cosh", "ctime", "div", "exit", "fabs", "floor", "fmod", "getchar", "getenv", "isalnum", "isalpha", "isdigit", "isgraph",
+    "ispunct", "isspace", "isupper", "kbhit", "log10", "log2", "log", "memcmp", "modf", "pow", "putchar", "putenv", "puts", "rand", "remove", "rename", "sinh", "sqrt", "srand", "strcat", "strcmp", "strerror", "time", "tolower", "toupper"
+  };
+  //}}}
+  //}}}
 
   //{{{
   bool isUtfSequence (char ch) {
@@ -2088,67 +2088,69 @@ void cTextEdit::addUndo (cUndo& undo) {
 
 //{{{
 void cTextEdit::parseLine (cLine& line) {
+// parse for fold stuff, tokenize and look for keyWords, builtins
 
   // glyphs to string
-  string text;
-  for (auto& glyph : line.mGlyphs)
-    text += glyph.mChar;
+  string textString;
+  for (auto& glyph : line.mGlyphs) {
+    glyph.mColor = eText;
+    textString += glyph.mChar;
+    }
 
   // look for foldBegin text
-  size_t foldBeginIndent = text.find (mOptions.mLanguage.mFoldBeginMarker);
+  size_t foldBeginIndent = textString.find (mOptions.mLanguage.mFoldBeginMarker);
   line.mFoldBegin = (foldBeginIndent != string::npos);
 
   if (line.mFoldBegin) {
-    // found foldBegin text, find ident
+    // found foldBegin textString, find ident
     line.mIndent = static_cast<uint8_t>(foldBeginIndent);
-    // has text after the foldBeginMarker
-    line.mComment = (text.size() != (foldBeginIndent + mOptions.mLanguage.mFoldBeginMarker.size()));
+    // has textString after the foldBeginMarker
+    line.mComment = (textString.size() != (foldBeginIndent + mOptions.mLanguage.mFoldBeginMarker.size()));
     mInfo.mHasFolds = true;
     }
   else {
     // normal line, find indent, find comment
-    size_t indent = text.find_first_not_of (' ');
+    size_t indent = textString.find_first_not_of (' ');
     if (indent != string::npos)
       line.mIndent = static_cast<uint8_t>(indent);
     else
       line.mIndent = 0;
 
-    // has "//" style comment as first text in line
-    line.mComment = (text.find (mOptions.mLanguage.mSingleLineComment, indent) != string::npos);
+    // has "//" style comment as first textString in line
+    line.mComment = (textString.find (mOptions.mLanguage.mSingleLineComment, indent) != string::npos);
     }
 
-  // look for foldEnd text
-  size_t foldEndIndent = text.find (mOptions.mLanguage.mFoldEndMarker);
+  // look for foldEnd textString
+  size_t foldEndIndent = textString.find (mOptions.mLanguage.mFoldEndMarker);
   line.mFoldEnd = (foldEndIndent != string::npos);
 
   // init fields set by updateFolds
   line.mFolded = true;
   line.mSeeThruOffset = 0;
 
+  // no point looking for tokens in empty line
   if (line.mGlyphs.empty())
     return;
 
-  string textString;
-  textString.resize (line.mGlyphs.size());
-  for (size_t j = 0; j < line.mGlyphs.size(); ++j) {
-    textString[j] = line.mGlyphs[j].mChar;
-    line.mGlyphs[j].mColor = eText;
-    }
-
+  // tokenise, look for keywords and builtins
   const char* bufferBegin = &textString.front();
   const char* bufferEnd = bufferBegin + textString.size();
   const char* lastChar = bufferEnd;
   for (const char* firstChar = bufferBegin; firstChar != lastChar; ) {
-    // iterate chars
+    //{{{  iterate chars
     const char* tokenBegin = nullptr;
     const char* tokenEnd = nullptr;
     uint8_t tokenColor = eText;
 
     bool hasTokenizeResult = false;
+
+    // try language tokenize
     if (mOptions.mLanguage.mTokenize != nullptr)
       if (mOptions.mLanguage.mTokenize (firstChar, lastChar, tokenBegin, tokenEnd, tokenColor))
         hasTokenizeResult = true;
+
     if (!hasTokenizeResult)
+      // try language regex
       for (auto& p : mOptions.mRegexList) {
         cmatch results;
         if (regex_search (firstChar, lastChar, results, p.first, regex_constants::match_continuous)) {
@@ -2161,23 +2163,23 @@ void cTextEdit::parseLine (cLine& line) {
           }
         }
 
-    if (!hasTokenizeResult)
-      firstChar++;
-    else {
+    if (hasTokenizeResult) {
+      // got token, look for keywords,builtins
       const size_t tokenLength = tokenEnd - tokenBegin;
       if (tokenColor == eBuiltIn) {
-        string id;
-        id.assign (tokenBegin, tokenEnd);
+        string tokenString (tokenBegin, tokenEnd);
 
         // almost all languages use lower case to specify keywords, so shouldn't this use ::tolower ?
         if (!mOptions.mLanguage.mCaseSensitive)
-          transform (id.begin(), id.end(), id.begin(),
-                     [](uint8_t ch) { return static_cast<uint8_t>(std::toupper (ch)); });
+          transform (tokenString.begin(), tokenString.end(), tokenString.begin(),
+            [](uint8_t ch) {
+              return static_cast<uint8_t>(std::toupper (ch));
+              });
 
         if (!line.mGlyphs[firstChar - bufferBegin].mPreProc) {
-          if (mOptions.mLanguage.mKeywords.count (id) != 0)
+          if (mOptions.mLanguage.mKeywords.count (tokenString) != 0)
             tokenColor = eKeyword;
-          else if (mOptions.mLanguage.mBuiltIns.count (id) != 0)
+          else if (mOptions.mLanguage.mBuiltIns.count (tokenString) != 0)
             tokenColor = eBuiltIn;
           }
         }
@@ -2187,7 +2189,11 @@ void cTextEdit::parseLine (cLine& line) {
 
       firstChar = tokenEnd;
       }
+
+    else
+      firstChar++;
     }
+    //}}}
   }
 //}}}
 
