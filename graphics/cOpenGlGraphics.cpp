f2b03e7af8ede9a263dd2c1a6e0d373af78182da
colinpaint <88828347+colinpaint@users.noreply.github.com>
2021-10-24 12:23:51 +0100

    more texture setup

--- graphics/cOpenGlGraphics.cpp
+++ graphics/cOpenGlGraphics.cpp
@@ -41,11 +41,15 @@ namespace {
       glGenTextures (1, &mTextureId);
 
       glBindTexture (GL_TEXTURE_2D, mTextureId);
-      glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-      glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
       glPixelStorei (GL_UNPACK_ROW_LENGTH, 0);
       glTexImage2D (GL_TEXTURE_2D, 0, GL_RGBA, size.x, size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+
+      glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+      glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+      glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+      glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+      glGenerateMipmap (GL_TEXTURE_2D);
       }
     //}}}
     //{{{
