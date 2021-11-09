614faa10ee47cf690bfb85de4f7df3ae44a74095
colinpaint <88828347+colinpaint@users.noreply.github.com>
2021-11-09 12:54:00 +0000

    add err string

--- dvb/cVideoRender.cpp
+++ dvb/cVideoRender.cpp
@@ -280,22 +280,26 @@ public:
       mfxVersion mfxVersion = {{0,1}};
       mfxStatus mfxStatus = mMfxSession.Init (mfxImpl, &mfxVersion);
       if (mfxStatus != MFX_ERR_NONE)
-        cLog::log (LOGINFO, fmt::format ("session.Init failed - status:{}", mfxStatus));
+        cLog::log (LOGINFO, fmt::format ("session.Init failed - status:{}:{}",
+                                         mfxStatus, errString (mfxStatus)));
 
       // query selected implementation and version
       mfxStatus = mMfxSession.QueryIMPL (&mfxImpl);
       if (mfxStatus != MFX_ERR_NONE)
-        cLog::log (LOGINFO, fmt::format ("QueryIMPL failed {}", mfxStatus));
+        cLog::log (LOGINFO, fmt::format ("QueryIMPL failed - status:{}:{}",
+                                         mfxStatus, errString (mfxStatus)));
 
       mfxStatus = mMfxSession.QueryVersion (&mfxVersion);
       if (mfxStatus != MFX_ERR_NONE)
-        cLog::log (LOGINFO, fmt::format ("QueryVersion failed {}", mfxStatus));
+        cLog::log (LOGINFO, fmt::format ("QueryVersion failed - status:{}:{}",
+                                         mfxStatus, errString (mfxStatus)));
 
-      cLog::log (LOGINFO, fmt::format ("mfxImpl:{:x}{}{}{} verMajor:{} verMinor:{}",
+      cLog::log (LOGINFO, fmt::format ("mfxImpl:{:x}{}{}{}{} verMajor:{} verMinor:{}",
                                        mfxImpl,
                                        (mfxImpl & MFX_IMPL_HARDWARE) ? " hw":"",
                                        (mfxImpl & MFX_IMPL_SOFTWARE) ? " sw":"",
                                        (mfxImpl & MFX_IMPL_VIA_D3D9) ? " d3d9":"",
+                                       (mfxImpl & MFX_IMPL_VIA_D3D11) ? " d3d11":"",
                                        mfxVersion.Major, mfxVersion.Minor));
       mSessionInited = true;
       }
@@ -311,14 +315,16 @@ public:
       mVideoParams.IOPattern = MFX_IOPATTERN_OUT_SYSTEM_MEMORY;
       mfxStatus mfxStatus = MFXVideoDECODE_DecodeHeader (mMfxSession, &mBitstream, &mVideoParams);
       if (mfxStatus != MFX_ERR_NONE)
-        cLog::log (LOGINFO, fmt::format ("MFXVideoDECODE_DecodeHeader failed - status:{}", mfxStatus));
+        cLog::log (LOGINFO, fmt::format ("MFXVideoDECODE_DecodeHeader failed - status:{}:{}",
+                                         mfxStatus, errString (mfxStatus)));
       else {
         //{{{  query surfaces
         mfxFrameAllocRequest frameAllocRequest;
         memset (&frameAllocRequest, 0, sizeof (frameAllocRequest));
         mfxStatus =  MFXVideoDECODE_QueryIOSurf (mMfxSession, &mVideoParams, &frameAllocRequest);
         if (mfxStatus != MFX_ERR_NONE)
-          cLog::log (LOGINFO, fmt::format ("MFXVideoDECODE_QueryIOSurf failed - status:{}", mfxStatus));
+          cLog::log (LOGINFO, fmt::format ("MFXVideoDECODE_QueryIOSurf failed - status:{}:{}",
+                                               mfxStatus, errString (mfxStatus)));
 
         mNumSurfaces = frameAllocRequest.NumFrameSuggested;
 
@@ -344,8 +350,8 @@ public:
 
         mfxStatus = MFXVideoDECODE_Init (mMfxSession, &mVideoParams);
         if (mfxStatus != MFX_ERR_NONE)
-          cLog::log (LOGINFO, fmt::format ("MFXVideoDECODE_Init failed - status:{}", mfxStatus));
-
+          cLog::log (LOGINFO, fmt::format ("MFXVideoDECODE_Init failed - status:{}:{}",
+                                           mfxStatus, errString (mfxStatus)));
         mInited = true;
         }
         //}}}
@@ -391,6 +397,35 @@ private:
           return i;
 
     return MFX_ERR_NOT_FOUND;
+    }
+  //}}}
+  //{{{
+  string errString (int mfxStatus) {
+
+    switch (mfxStatus) {
+      case   0: return "No error";
+      case  -1: return "Unknown error";
+      case  -2: return "Null pointer";
+      case  -3: return "Unsupported feature/library load error";
+      case  -4: return "Could not allocate memory" ;
+      case  -5: return "Insufficient IO buffers" ;
+      case  -6: return "Invalid handle";
+      case  -7: return "Memory lock failure" ;
+      case  -8: return "Function called before initialization" ;
+      case  -9: return "Specified object not found" ;
+      case -10: return "More input data expected";
+      case -11: return "More output surfaces expected";
+      case -12: return "Operation aborted" ;
+      case -13: return "HW device lost";
+      case -14: return "Incompatible video parameters" ;
+      case -15: return "Invalid video parameters";
+      case -16: return "Undefined behavior";
+      case -17: return "Device operation failure";
+      case -18: return "More bitstream data expected";
+      case -19: return "Incompatible audio parameters";
+      case -20: return "Invalid audio parameters" ;
+      default: return"Error code";
+      }
     }
   //}}}
 
--- mfxtest/main.cpp
+++ mfxtest/main.cpp
@@ -11,6 +11,36 @@
 using namespace std;
 //}}}
 
+//{{{
+string errString (int mfxStatus) {
+
+  switch (mfxStatus) {
+    case   0: return "No error";
+    case  -1: return "Unknown error";
+    case  -2: return "Null pointer";
+    case  -3: return "Unsupported feature/library load error";
+    case  -4: return "Could not allocate memory" ;
+    case  -5: return "Insufficient IO buffers" ;
+    case  -6: return "Invalid handle";
+    case  -7: return "Memory lock failure" ;
+    case  -8: return "Function called before initialization" ;
+    case  -9: return "Specified object not found" ;
+    case -10: return "More input data expected";
+    case -11: return "More output surfaces expected";
+    case -12: return "Operation aborted" ;
+    case -13: return "HW device lost";
+    case -14: return "Incompatible video parameters" ;
+    case -15: return "Invalid video parameters";
+    case -16: return "Undefined behavior";
+    case -17: return "Device operation failure";
+    case -18: return "More bitstream data expected";
+    case -19: return "Incompatible audio parameters";
+    case -20: return "Invalid audio parameters" ;
+    default: return"Error code";
+    }
+  }
+//}}}
+
 int main(int argc, char** argv) {
 
   (void)argc;
@@ -18,28 +48,36 @@ int main(int argc, char** argv) {
   cLog::init (LOGINFO);
   cLog::log (LOGNOTICE, fmt::format ("mfxtest - decode"));
 
-  mfxIMPL mfxImpl = MFX_IMPL_AUTO_ANY;
+  //{{{  MFX_IMPL flags
+  // MFX_IMPL_AUTO
+  // MFX_IMPL_HARDWARE
+  // MFX_IMPL_SOFTWARE
+  // MFX_IMPL_AUTO_ANY
+  // MFX_IMPL_VIA_D3D9
+  // MFX_IMPL_VIA_D3D11
+  //}}}
+  mfxIMPL mfxImpl = MFX_IMPL_AUTO_ANY | MFX_IMPL_VIA_D3D11;
   mfxVersion mfxVersion = {{0,1}};
   MFXVideoSession mfxSession;
   mfxStatus mfxStatus = mfxSession.Init (mfxImpl, &mfxVersion);
   if (mfxStatus != MFX_ERR_NONE)
-    cLog::log (LOGINFO, fmt::format ("session.Init failed {}", mfxStatus));
+    cLog::log (LOGINFO, fmt::format ("session.Init failed - status:{}:{}", mfxStatus, errString(mfxStatus)));
 
   //{{{  Query selected implementation and version
   mfxStatus = mfxSession.QueryIMPL (&mfxImpl);
   if (mfxStatus != MFX_ERR_NONE)
-    cLog::log (LOGINFO, fmt::format ("QueryIMPL failed {}", mfxStatus));
+    cLog::log (LOGINFO, fmt::format ("QueryIMPL failed - status:{}:{}", mfxStatus, errString(mfxStatus)));
   mfxStatus = mfxSession.QueryVersion (&mfxVersion);
   if (mfxStatus != MFX_ERR_NONE)
-    cLog::log (LOGINFO, fmt::format ("QueryVersion failed {}", mfxStatus));
+    cLog::log (LOGINFO, fmt::format ("QueryVersion failed - status:{}:{}}", mfxStatus, errString(mfxStatus)));
 
-  cLog::log (LOGINFO, fmt::format ("mfxImpl:{:x}{}{}{} verMajor:{} verMinor:{}",
+  cLog::log (LOGINFO, fmt::format ("mfxImpl:{:x}{}{}{}{} verMajor:{} verMinor:{}",
                                    mfxImpl,
                                    (mfxImpl & MFX_IMPL_HARDWARE) ? " hw":"",
                                    (mfxImpl & MFX_IMPL_SOFTWARE) ? " sw":"",
                                    (mfxImpl & MFX_IMPL_VIA_D3D9) ? " d3d9":"",
+                                   (mfxImpl & MFX_IMPL_VIA_D3D11) ? " d3d11":"",
                                    mfxVersion.Major, mfxVersion.Minor));
-
   //}}}
   mfxSession.Close();
 
